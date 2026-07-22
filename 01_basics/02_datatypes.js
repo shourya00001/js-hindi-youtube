07049de6295ee1db77e437d20a223dfb6edf681c
shourya00001 <2k23.aids2312112@gmail.com>
2026-07-22 17:33:14 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+
+//alert(3 + 3) we are using node js, not browser
+
+console.log( 3 +
+    3
+) //code readibility should be high
+
+let name = "shourya"
+let age = 21
+let isLoggedin = false
+
+//number => 2 to thr power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof object);
