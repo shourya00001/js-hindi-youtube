02cf3d4ee9d691e3bc91ccdd6d3359e14db4fd96
shourya00001 <2k23.aids2312112@gmail.com>
2026-07-29 10:47:52 +0000

    commit

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,63 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1; false => 0;
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn); 
+
+//1 => true
+//null => false
+//"shourya" => true
+
+let somenumber = 33
+let Stringnumber = String(somenumber);
+// console.log(Stringnumber);
+// console.log(typeof (Stringnumber));
+
+// **********************************OPERATIONS***********************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2**2)
+// console.log(2*2)
+// console.log(2%3)
+// console.log(2/2)
+
+let str1 = "shourya"
+let str2 = "hello"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log((3 + 4) * 5 % 3)
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter ++;
+console.log (gamecounter);
